6cbe398bb1ecf7f860a7f4b2db12e583ebb9c169
Enstso <enstso@outlook.fr>
2024-01-19 13:09:09 +0100

    starter-react

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+// components/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <p>Bienvenue sur la page d'accueil!</p>
+    </div>
+  );
+};
+
+export default Home;
